18bcc0ac3812805c837275464c5169ec3b85ffc8
Marco Buono <thecoreh@gmail.com>
2017-08-26 00:53:56 -0300

    Parametrize isosurface generation

--- src/field.rs
+++ src/field.rs
@@ -4,32 +4,37 @@ pub trait Isosurface {
     fn isosurface<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a)) -> Self;
 }
 
+const COUNT: i32 = 32;
+const STEP: f64 = 1.0;
+const HALF: f64 = STEP / 2.0;
+const ____: f64 = 0.0;
+
 impl Isosurface for Geometry {
     fn isosurface<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a)) -> Geometry {
         let mut data = Vec::<Vertex>::with_capacity(50000);
-        for x in 0..32 {
-            for y in 0..32 {
-                for z in 0..32 {
-                    let x = f64::from(x);
-                    let y = f64::from(y);
-                    let z = f64::from(z);
+        for x in 0..COUNT {
+            for y in 0..COUNT {
+                for z in 0..COUNT {
+                    let x = f64::from(x) * STEP;
+                    let y = f64::from(y) * STEP;
+                    let z = f64::from(z) * STEP;
                     if test(field, x, y, z) {
-                        if !test(field, x, y, z - 1.0) {
+                        if !test(field, x, y, z - STEP) {
                             data.extend_from_slice(&back(field, x, y, z));
                         }
-                        if !test(field, x, y, z + 1.0) {
+                        if !test(field, x, y, z + STEP) {
                             data.extend_from_slice(&front(field, x, y, z));
                         }
-                        if !test(field, x - 1.0, y, z) {
+                        if !test(field, x - STEP, y, z) {
                             data.extend_from_slice(&left(field, x, y, z));
                         }
-                        if !test(field, x + 1.0, y, z) {
+                        if !test(field, x + STEP, y, z) {
                             data.extend_from_slice(&right(field, x, y, z));
                         }
-                        if !test(field, x, y - 1.0, z) {
+                        if !test(field, x, y - STEP, z) {
                             data.extend_from_slice(&bottom(field, x, y, z));
                         }
-                        if !test(field, x, y + 1.0, z) {
+                        if !test(field, x, y + STEP, z) {
                             data.extend_from_slice(&top(field, x, y, z));
                         }
                     }
@@ -42,38 +47,39 @@ impl Isosurface for Geometry {
 
 #[inline]
 fn test<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> bool {
-    field(x + 0.5, y + 0.5, z + 0.5) < 0.0
+    field(x + HALF, y + HALF, z + HALF) < 0.0
 }
 
 #[inline]
 fn vertex<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> Vertex {
-    let x_a = field(x - 1.0, y, z).abs();
-    let x_b = field(x + 1.0, y, z).abs();
+    let x_a = field(x - STEP, y + ____, z + ____).abs();
+    let x_b = field(x + STEP, y + ____, z + ____).abs();
     let x = if x_a + x_b > 0.0 {
-        (x + 1.0) * x_a / (x_a + x_b) + (x - 1.0) * x_b / (x_a + x_b)
+        (x + STEP) * x_a / (x_a + x_b) + (x - STEP) * x_b / (x_a + x_b)
     } else {
         x
     };
 
-    let y_a = field(x, y - 1.0, z).abs();
-    let y_b = field(x, y + 1.0, z).abs();
+    let y_a = field(x + ____, y - STEP, z + ____).abs();
+    let y_b = field(x + ____, y + STEP, z + ____).abs();
     let y = if y_a + y_b > 0.0 {
-        (y + 1.0) * y_a / (y_a + y_b) + (y - 1.0) * y_b / (y_a + y_b)
+        (y + STEP) * y_a / (y_a + y_b) + (y - STEP) * y_b / (y_a + y_b)
     } else {
         y
     };
 
-    let z_a = field(x, y, z - 1.0).abs();
-    let z_b = field(x, y, z + 1.0).abs();
+    let z_a = field(x + ____, y + ____, z - STEP).abs();
+    let z_b = field(x + ____, y + ____, z + STEP).abs();
     let z = if z_a + z_b > 0.0 {
-        (z + 1.0) * z_a / (z_a + z_b) + (z - 1.0) * z_b / (z_a + z_b)
+        (z + STEP) * z_a / (z_a + z_b) + (z - STEP) * z_b / (z_a + z_b)
     } else {
         z
     };
 
-    let n_x = field(x + 0.5, y, z) - field(x - 0.5, y, z);
-    let n_y = field(x, y + 0.5, z) - field(x, y - 0.5, z);
-    let n_z = field(x, y, z + 0.5) - field(x, y, z - 0.5);
+    let n_x = field(x + HALF, y + ____, z + ____) - field(x - HALF, y + ____, z + ____);
+    let n_y = field(x + ____, y + HALF, z + ____) - field(x + ____, y - HALF, z + ____);
+    let n_z = field(x + ____, y + ____, z + HALF) - field(x + ____, y + ____, z - HALF);
+
     let l = (n_x.powi(2) + n_y.powi(2) + n_z.powi(2)).sqrt();
 
     Vertex {
@@ -93,62 +99,63 @@ fn vertex_blocky(field: &Field, x: f64, y: f64, z: f64, a: f32, b: f32, c: f32) 
     }
 }
 */
+
 #[inline]
 fn front<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 0.0, y + 0.0, z + 1.0),
-     vertex(field, x + 1.0, y + 0.0, z + 1.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0),
-     vertex(field, x + 0.0, y + 0.0, z + 1.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0),
-     vertex(field, x + 0.0, y + 1.0, z + 1.0)]
+    [vertex(field, x + ____, y + ____, z + STEP),
+     vertex(field, x + STEP, y + ____, z + STEP),
+     vertex(field, x + STEP, y + STEP, z + STEP),
+     vertex(field, x + ____, y + ____, z + STEP),
+     vertex(field, x + STEP, y + STEP, z + STEP),
+     vertex(field, x + ____, y + STEP, z + STEP)]
 }
 
 #[inline]
 fn back<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 0.0, y + 0.0, z),
-     vertex(field, x + 1.0, y + 1.0, z),
-     vertex(field, x + 1.0, y + 0.0, z),
-     vertex(field, x + 1.0, y + 1.0, z),
-     vertex(field, x + 0.0, y + 0.0, z),
-     vertex(field, x + 0.0, y + 1.0, z)]
+    [vertex(field, x + ____, y + ____, z),
+     vertex(field, x + STEP, y + STEP, z),
+     vertex(field, x + STEP, y + ____, z),
+     vertex(field, x + STEP, y + STEP, z),
+     vertex(field, x + ____, y + ____, z),
+     vertex(field, x + ____, y + STEP, z)]
 }
 
 #[inline]
 fn right<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 1.0, y + 0.0, z + 0.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0),
-     vertex(field, x + 1.0, y + 0.0, z + 1.0),
-     vertex(field, x + 1.0, y + 0.0, z + 0.0),
-     vertex(field, x + 1.0, y + 1.0, z + 0.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0)]
+    [vertex(field, x + STEP, y + ____, z + ____),
+     vertex(field, x + STEP, y + STEP, z + STEP),
+     vertex(field, x + STEP, y + ____, z + STEP),
+     vertex(field, x + STEP, y + ____, z + ____),
+     vertex(field, x + STEP, y + STEP, z + ____),
+     vertex(field, x + STEP, y + STEP, z + STEP)]
 }
 
 #[inline]
 fn left<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 0.0, y + 0.0, z + 0.0),
-     vertex(field, x + 0.0, y + 0.0, z + 1.0),
-     vertex(field, x + 0.0, y + 1.0, z + 1.0),
-     vertex(field, x + 0.0, y + 0.0, z + 0.0),
-     vertex(field, x + 0.0, y + 1.0, z + 1.0),
-     vertex(field, x + 0.0, y + 1.0, z + 0.0)]
+    [vertex(field, x + ____, y + ____, z + ____),
+     vertex(field, x + ____, y + ____, z + STEP),
+     vertex(field, x + ____, y + STEP, z + STEP),
+     vertex(field, x + ____, y + ____, z + ____),
+     vertex(field, x + ____, y + STEP, z + STEP),
+     vertex(field, x + ____, y + STEP, z + ____)]
 }
 
 #[inline]
 fn top<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 0.0, y + 1.0, z + 0.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0),
-     vertex(field, x + 1.0, y + 1.0, z + 0.0),
-     vertex(field, x + 0.0, y + 1.0, z + 0.0),
-     vertex(field, x + 0.0, y + 1.0, z + 1.0),
-     vertex(field, x + 1.0, y + 1.0, z + 1.0)]
+    [vertex(field, x + ____, y + STEP, z + ____),
+     vertex(field, x + STEP, y + STEP, z + STEP),
+     vertex(field, x + STEP, y + STEP, z + ____),
+     vertex(field, x + ____, y + STEP, z + ____),
+     vertex(field, x + ____, y + STEP, z + STEP),
+     vertex(field, x + STEP, y + STEP, z + STEP)]
 }
 
 #[inline]
 fn bottom<'a>(field: &(Fn(f64, f64, f64) -> f64 + 'a), x: f64, y: f64, z: f64) -> [Vertex; 6] {
-    [vertex(field, x + 0.0, y + 0.0, z + 0.0),
-     vertex(field, x + 1.0, y + 0.0, z + 1.0),
-     vertex(field, x + 0.0, y + 0.0, z + 1.0),
-     vertex(field, x + 1.0, y + 0.0, z + 1.0),
-     vertex(field, x + 0.0, y + 0.0, z + 0.0),
-     vertex(field, x + 1.0, y + 0.0, z + 0.0)]
+    [vertex(field, x + ____, y + ____, z + ____),
+     vertex(field, x + STEP, y + ____, z + STEP),
+     vertex(field, x + ____, y + ____, z + STEP),
+     vertex(field, x + STEP, y + ____, z + STEP),
+     vertex(field, x + ____, y + ____, z + ____),
+     vertex(field, x + STEP, y + ____, z + ____)]
 }
